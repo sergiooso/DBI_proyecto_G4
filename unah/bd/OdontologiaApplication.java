fe4921dec6730dd2e2a720f0bf46aed02e8f7dc5
ChessnuttGriffin <74697783+ChessnuttGriffin@users.noreply.github.com>
2021-08-01 18:20:36 -0600

    Controlador hecho
    Faltan los demas metodos, probar conexion (Se puede hacer con lo que esta ya hecho)

---
+++ unah/bd/OdontologiaApplication.java
@@ -0,0 +1,13 @@
+package edu.unah.bd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdontologiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdontologiaApplication.class, args);
+	}
+
+}
---
+++ unah/repository/RepositorySucursal.java
@@ -0,0 +1,9 @@
+package edu.unah.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import edu.unah.model.Sucursal;
+
+public interface RepositorySucursal extends JpaRepository<Sucursal, Integer>{
+	public Sucursal findById(int id);
+}
