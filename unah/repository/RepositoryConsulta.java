ce93e8fd94024447526fb61269061d59405020f3
ChessnuttGriffin <74697783+ChessnuttGriffin@users.noreply.github.com>
2021-08-09 18:13:29 -0600

    Delete unah directory

--- unah/repository/RepositoryConsulta.java
+++
@@ -1,9 +0,0 @@
-package edu.unah.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import edu.unah.model.Consulta;
-
-public interface RepositoryConsulta extends JpaRepository<Consulta, Integer>{
-	public Consulta findById(int id);
-}
--- unah/repository/RepositoryTratamiento.java
+++
@@ -1,9 +0,0 @@
-package edu.unah.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import edu.unah.model.Tratamiento;
-
-public interface RepositoryTratamiento extends JpaRepository<Tratamiento, Integer>{
-	public Tratamiento findById(int id);
-}
